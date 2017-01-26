fdd04b00d30705d60f49329a190cc871cd4e1aee
Juan Luna Bejarano <jualunab@coboglbw01234.global.publicisgroupe.net>
2017-01-26 17:02:00 -0500

    Entorno reestructurado con grunt.watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,27 +26,48 @@ module.exports = function(grunt) {
         },
         browserify : {
             dist: {
-                options: {
-                    transform: [['babelify', {presets: ['es2015']}]]
-                },
                 files: {
                     // if the source file has an extension of es6 then
                     // we change the name of the source file accordingly.
                     // The result file's extension is always .js
                     "./public/lib/scripts/module.js":
                         ["./lib/scripts/index.js"]
+                },
+                options: {
+                    transform: [['babelify', {presets: ['es2015']}]]
                 }
             }
         },
         pug: {
             compile: {
-                options: {
-                    data: {
-                        debug: false
-                    }
+                options : {
+                  pretty : true
                 },
                 files: {
                     'index.html': ['views/index.pug']
+                }
+            }
+        },
+        watch : {
+            scripts: {
+                files: ['lib/**/*.js'],
+                tasks: ['browserify'],
+                options: {
+                    spawn: false,
+                }
+            },
+            css : {
+                files: ['lib/**/*.scss'],
+                tasks: ['sass', 'copy'],
+                options: {
+                    spawn: false,
+                }
+            },
+            html : {
+                files: ['views/*.pug'],
+                tasks: ['pug'],
+                options: {
+                    spawn: false,
                 }
             }
         }
@@ -57,7 +78,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass', 'copy', 'browserify', 'pug']);
+    grunt.registerTask('default', ['sass', 'browserify', 'pug', 'copy']);
 };
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.render('index');
 })
+app.get('/signup', function (req, res) {
+    res.render('index');
+})
+app.get('/signin', function (req, res) {
+    res.render('index');
+})
 
 app.listen(3000, function (err) {
     if (err) return console.log('Hubo un error'), process.exit(1);
